b8ee9ac2e51212011484fd4bae2d762bc9022cdf
Charan Karthik <secharank@gmail.com>
2022-07-30 23:44:10 -0700

    first assignment

---
+++ Practice Assignments/First Java Program/PrintInfo.java
@@ -0,0 +1,9 @@
+
+
+public class PrintInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Charan Karthik.");
+        System.out.println("As of writing this code, I am 22 years old.");
+        System.out.println("My hometown is near Sacramento, CA.");
+	}
+}
---
+++ Practice Assignments/greetings/Test.java
@@ -0,0 +1,22 @@
+// File name must be the same as the class name. Every file is a class.
+
+public class Test {
+
+    // The file or class with the exact method signature below
+    // is always the launching point of the application
+    // You cannot run a Java program without it.
+
+	public static void main(String[] args) {
+        // For now, all code will go in here
+        System.out.println("Hello world!"); // Semicolons are REQUIRED! Not like JS where it's optional
+	}
+}
+
+// Use PascalCase when working with Java (i.e. BankAccount instead of bankAccount or bank_account)
+
+// Compile with javac "FileName.java"
+// Compiling will create a .class file
+// Run with java "FileName"
+
+// Whenever you run a file, you gotta compile!
+// Every time you make a change to your code, you will need to re-compile it.
