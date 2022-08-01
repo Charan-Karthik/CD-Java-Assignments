a543cee9e324adfa1da1e5cb0c2a85d3207da683
Charan Karthik <secharank@gmail.com>
2022-08-01 16:50:17 -0700

    completed all methods w/o challenges (yet)

--- Practice Assignments/Puzzling/PuzzleJava.java
+++ Practice Assignments/Puzzling/PuzzleJava.java
@@ -1,5 +1,46 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 public class PuzzleJava {
-    public String testing(){
-        return "Testing";
+    Random randMachine = new Random();
+
+    public ArrayList<Integer> getTenRolls(){
+        ArrayList<Integer> tenRandomNumbers = new ArrayList<Integer>();
+        for(int i=0; i<10; i++){
+            int x = randMachine.nextInt(21);
+            // System.out.println("i = " + i + " & x = " + x);
+            if(x!=0){
+                tenRandomNumbers.add(x);
+            } else {
+                i--;
+            }
+        }
+        return tenRandomNumbers;
+    }
+
+    public char getRandomLetter(){
+        char[] letterArray = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        int randomLetterPosition = randMachine.nextInt(26);
+        return letterArray[randomLetterPosition];
+    }
+
+    public String generatePassword(){
+        String password = "";
+        for(int i=0; i<8; i++){
+            password += getRandomLetter();
+        }
+        return password;
+    }
+
+    public ArrayList<String> getNewPasswordSet(int numberOfPasswords){
+        if(numberOfPasswords == 0){
+            numberOfPasswords += 1;
+        }
+
+        ArrayList<String> passwordsArray = new ArrayList<String>();
+        for(int i=0; i<numberOfPasswords; i++){
+            passwordsArray.add(generatePassword());
+        }
+    return passwordsArray;
     }
 }
--- Practice Assignments/Puzzling/TestPuzzle.java
+++ Practice Assignments/Puzzling/TestPuzzle.java
@@ -1,7 +1,14 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 public class TestPuzzle {
     public static void main(String[] args){
         PuzzleJava puzzleMethods = new PuzzleJava();
-        
-        System.out.println(puzzleMethods.testing());
+        Random randMachine = new Random();
+
+        System.out.println(puzzleMethods.getTenRolls());
+        System.out.println(puzzleMethods.getRandomLetter());
+        System.out.println(puzzleMethods.generatePassword());
+        System.out.println(puzzleMethods.getNewPasswordSet(randMachine.nextInt(10)));
     }
 }
