21797b6951f8936d4cee038e600add4634ae2c94
Charan Karthik <secharank@gmail.com>
2022-08-01 14:14:53 -0700

    save methods added

--- Core Assignments/Cafe Business Logic/CafeUtil.java
+++ Core Assignments/Cafe Business Logic/CafeUtil.java
@@ -1,3 +1,18 @@
 public class CafeUtil {
     
+    public int getStreakGoal(int numWeeks){
+        int sum = 0;
+        for(int i=0; i <= numWeeks; i++){
+            sum += i;
+        }
+        return sum;
+    }
+
+    public double getOrderTotal(double[] prices){
+        double total = 0;
+        for(int i=0; i<prices.length; i++){
+            total += prices[i];
+        }
+        return total;
+    }
 }
--- Core Assignments/Cafe Business Logic/TestCafe.java
+++ Core Assignments/Cafe Business Logic/TestCafe.java
@@ -1,5 +1,11 @@
 public class TestCafe {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+        CafeUtil cafeMethods = new CafeUtil();
+
+        System.out.println(cafeMethods.getStreakGoal(10));
+
+        double[] itemPrices = {2.5, 3.5, 4.5, 5.5};
+        System.out.println(cafeMethods.getOrderTotal(itemPrices));
 	}
 }
