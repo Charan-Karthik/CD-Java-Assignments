dfd301e5c01f03ae5c09cb31b70a1cd792a5c3ea
Charan Karthik <secharank@gmail.com>
2022-08-01 14:29:42 -0700

    save progress

--- Core Assignments/Cafe Business Logic/TestCafe.java
+++ Core Assignments/Cafe Business Logic/TestCafe.java
@@ -1,11 +1,25 @@
+import java.util.ArrayList;
+
 public class TestCafe {
 	public static void main(String[] args) {
 		
-        CafeUtil cafeMethods = new CafeUtil();
+        // CafeUtil cafeMethods = new CafeUtil();
 
-        System.out.println(cafeMethods.getStreakGoal(10));
+        // System.out.println(cafeMethods.getStreakGoal(10));
 
-        double[] itemPrices = {2.5, 3.5, 4.5, 5.5};
-        System.out.println(cafeMethods.getOrderTotal(itemPrices));
+        // double[] itemPrices = {2.5, 3.5, 4.5, 5.5};
+        // System.out.println(cafeMethods.getOrderTotal(itemPrices));
+
+        // ArrayList<String> menuItems = new ArrayList<String>();
+        // menuItems.add("drip coffee");
+        // menuItems.add("cappuccino");
+        // menuItems.add("latte");
+        // menuItems.add("mocha");
+        // cafeMethods.displayMenu(menuItems);
+
+        // ArrayList<String> customers = new ArrayList<String>();
+        // cafeMethods.addCustomer(customers);
+
+        
 	}
 }
