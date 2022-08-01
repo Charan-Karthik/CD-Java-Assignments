533e39d7696e7fd49d0f47a46d2c1393476f4eb8
Charan Karthik <secharank@gmail.com>
2022-08-01 14:55:59 -0700

    all tests completed without bonuses

--- Core Assignments/Cafe Business Logic/TestCafe.java
+++ Core Assignments/Cafe Business Logic/TestCafe.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 public class TestCafe {
-	public static void main(String[] args) {
-		
-        // CafeUtil cafeMethods = new CafeUtil();
+    public static void main(String[] args){
+
+        CafeUtil cafeMethods = new CafeUtil();
 
         // System.out.println(cafeMethods.getStreakGoal(10));
 
@@ -20,6 +20,30 @@ public class TestCafe {
         // ArrayList<String> customers = new ArrayList<String>();
         // cafeMethods.addCustomer(customers);
 
+        /* ============ App Test Cases ============= */
+    
+        System.out.println("\n----- Streak Goal Test -----");
+        System.out.printf("Purchases needed by week 10: %s \n\n", cafeMethods.getStreakGoal(10));
+    
+        System.out.println("----- Order Total Test-----");
+        double[] lineItems = {3.5, 1.5, 4.0, 4.5};
+        System.out.printf("Order total: %s \n\n", cafeMethods.getOrderTotal(lineItems));
         
-	}
-}
+        System.out.println("----- Display Menu Test-----");
+        
+        ArrayList<String> menu = new ArrayList<String>();
+        menu.add("drip coffee");
+        menu.add("cappuccino");
+        menu.add("latte");
+        menu.add("mocha");
+        cafeMethods.displayMenu(menu);
+    
+        System.out.println("\n----- Add Customer Test-----");
+        ArrayList<String> customers = new ArrayList<String>();
+        // --- Test 4 times ---
+        for (int i = 0; i < 4; i++) {
+            cafeMethods.addCustomer(customers);
+            System.out.println("\n");
+        }
+    }
+} 
