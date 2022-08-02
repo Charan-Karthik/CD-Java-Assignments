c5396f41546691029c7a9d789aaeb7be2c25d8dd
Charan Karthik <secharank@gmail.com>
2022-08-02 13:42:39 -0700

    initial commit

---
+++ Practice Assignments/Barista's Challenge/Item.java
@@ -0,0 +1,30 @@
+public class Item {
+    
+    // MEMBER VARIABLES
+    private String name;
+    private double price;
+    
+    // CONSTRUCTOR
+    //   Takes a name and price as arguments 
+    //   and sets them accordingly
+    public Item(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+    
+    // GETTERS
+    public String getName(){
+        return this.name;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+
+    // SETTERS
+    public void String setName(String name){
+        this.name = name;
+    }
+    public void double setPrice(double price){
+        this.price = price;
+    }
+}
