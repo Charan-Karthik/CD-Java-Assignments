1685937532d9bf82b6058596730b5ab343652d3b
Charan Karthik <secharank@gmail.com>
2022-08-05 11:52:35 -0700

    daikichi routes

---
+++ Practice Assignments/Daikichi Routes/src/main/java/com/charan/daikichiroutes/controllers/DaikichiController.java
@@ -0,0 +1,29 @@
+package com.charan.daikichiroutes.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DaikichiController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "";
+	}
+	
+	@RequestMapping("/daikichi")
+	public String daikichi() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/daikichi/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("/daikichi/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+}
