1f65549b1310866e22d94d3426f94109c273289c
Charan Karthik <secharank@gmail.com>
2022-08-09 13:54:48 -0700

    Books API (extended from lecture)

---
+++ Practice Assignments/Books API/src/main/java/com/charan/apicrud/W2d2MorningApiApplication.java
@@ -0,0 +1,13 @@
+package com.charan.apicrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2d2MorningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W2d2MorningApiApplication.class, args);
+	}
+
+}
---
+++ Practice Assignments/Books API/src/test/java/com/charan/apicrud/W2d2MorningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charan.apicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W2d2MorningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
